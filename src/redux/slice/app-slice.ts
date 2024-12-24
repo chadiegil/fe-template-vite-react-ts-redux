66c7c1e3212d61e57bc1cff5e3af5f0526867b1f
Chadie Gil <caugis@gmail.com>
2024-12-24 17:37:13 +0800

    update: typo api.ts

---
+++ src/redux/slice/app-slice.ts
@@ -0,0 +1,17 @@
+// import {createSlice} from "@reduxjs/toolkit"
+
+// // interface InitialState{
+
+// // }
+
+// const app = createSlice({
+//     name:"app",
+//     // initialState, add this if you have state
+//     reducers:{
+
+//     }
+// })
+
+// export const {} = appSlice.actions
+
+// export default appSlice.reducer
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,4 +2,4 @@ import { axiosPublic } from "@/utils/axios-public"
 // import { axiosInstance } from "@utils/axios-instance"
 
 export const refreshUserToken = async () =>
-  await axiosPublic.get("/auth/refres")
+  await axiosPublic.get("/auth/refresh")
